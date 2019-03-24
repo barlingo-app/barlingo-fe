91ac63b1a59518269f6a59ab2facc453928ec695
José <dragojola@gmail.com>
2019-03-24 19:30:23 +0100

    SeeEstablishment

---
+++ src/components/CustomCard/CustomCard.js
@@ -0,0 +1,81 @@
+import React, { Component } from 'react';
+import { withNamespaces } from "react-i18next";
+import './CustomCard.scss'
+import barLogo from '../../media/photo-1514933651103-005eec06c04b.jpg'
+import locationIcon from '../../media/imageedit_5_5395394410.png'
+import timeIcon from '../../media/imageedit_8_4988666292.png'
+import personIcon from '../../media/person.png'
+import spanish from '../../media/spain.svg'
+import english from '../../media/united-kingdom.svg'
+import french from '../../media/france.svg'
+import german from '../../media/germany.svg'
+import { NavLink } from "react-router-dom";
+
+class CustomCard extends Component {
+
+    getRandomArbitrary(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    }
+    renderLanguageWrapper() {
+        let max = this.props.max;
+
+        if (max) {
+
+            let i = this.getRandomArbitrary(0, 4);
+            let j = this.getRandomArbitrary(0, 3);
+
+            let array = [spanish, english, french, german];
+            let motherTongue = array[i];
+            array.splice(i, 1);
+            let targetLanguage = array[j];
+
+            return (<div className="custom-card__language-wrapper">
+                <img className="custom-card__language-icon" src={motherTongue} alt="Mother tongue" />
+                <img className="custom-card__language-icon" src={targetLanguage} alt="Target language" />
+            </div>);
+        }
+        return null;
+    }
+    renderParticipants() {
+        let max = this.props.max;
+        if (max)
+            return (<div className="custom-card__participants-wrapper">
+                <img className="custom-card__participants-icon" src={personIcon} alt="Participants" />
+                <p className="custom-card__text">{max}</p>
+            </div>);
+        return null;
+    }
+
+    render() {
+        let image = this.props.image;
+        let title = this.props.title;
+        let route = this.props.route;
+        let address = this.props.address;
+        let schedule = this.props.schedule;
+        let buttonMessage = this.props.buttonMessage;
+        return (
+            <div>
+                <div className="custom-card">
+                    <img className="custom-card__image" src={image} alt="Bar logo" />
+                    <p className="custom-card__title">
+                        <NavLink activeClassName={"active"} to={`/${route}/${title}`}>{title}</NavLink>
+                    </p>
+
+                    {this.renderLanguageWrapper()}
+                    <div className="custom-card__location-wrapper">
+                        <img className="custom-card__location-icon" src={locationIcon} alt="Location" />
+                        <p className="custom-card__text">{address}</p>
+                    </div>
+                    <div className="custom-card__time-wrapper">
+                        <img className="custom-card__time-icon" src={timeIcon} alt="Date and time" />
+                        <p className="custom-card__text">{schedule}</p>
+                    </div>
+                    {this.renderParticipants()}
+                    <div className="custom-card__button-wrapper"><button className="custom-card__button">{buttonMessage}</button></div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default withNamespaces('translation')(CustomCard);
---
+++ src/containers/LoginForm/LoginForm.js
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {
+    Form, Icon, Input, Button, Checkbox,
+} from 'antd';
+import { Page, Section } from "react-page-layout";
+import 'antd/dist/antd.css';
+import { withNamespaces } from "react-i18next";
+
+class LoginForm extends Component {
+
+    handleSubmit = (e) => {
+        e.preventDefault();
+        this.props.form.validateFields((err, values) => {
+            if (!err) {
+                console.log('Received values of form: ', values);
+            }
+        });
+    }
+    render() {
+        const { t } = this.props;
+        const { getFieldDecorator } = this.props.form;
+        return (
+            <Page layout="public">
+                <Section slot="content">
+                    <Form onSubmit={this.handleSubmit} className="login-form">
+                        <Form.Item>
+                            {getFieldDecorator('userName', {
+                                rules: [{ required: true, message: t('form.emptyUsername') }],
+                            })(
+                                <Input prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />} placeholder={t('form.username')} />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('password', {
+                                rules: [{ required: true, message: t('form.emptyPassword') }],
+                            })(
+                                <Input prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="password" placeholder={t('form.password')} />
+                            )}
+                        </Form.Item>
+                        <Form.Item>
+                            {getFieldDecorator('remember', {
+                                valuePropName: 'checked',
+                                initialValue: true,
+                            })(
+                                <Checkbox>{t('form.remember')}</Checkbox>
+                            )}
+                            <a className="login-form-forgot" href="/">{t('form.forgotPassword')}</a>
+                            <Button type="primary" htmlType="submit" className="login-form-button">
+                                {t('form.login')}
+                            </Button>
+                            <a href="/">{t('form.register')}</a>
+                        </Form.Item>
+                    </Form>
+                </Section>
+            </Page>
+
+
+        );
+    }
+}
+LoginForm = Form.create({ name: "normal_login" })(LoginForm)
+
+export default withNamespaces('translation')(LoginForm);
