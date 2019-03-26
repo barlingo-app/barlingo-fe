bd7de6a58b331cd227f1ff5ad68cad37bcad7592
Jesús Rivas Jiménez <jesus.rivas.jmnez@gmail.com>
2019-03-26 21:16:43 +0100

    @jesusrivasjmnez - CI/CD system
    * Update scripts to upload build package to UAT/PRD server.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,9 +4,25 @@ NOW=$(date +"%Y-%m-%d")
 
 if [ "$TRAVIS_BRANCH" == "CI" ]; then
   echo "Deploying $TRAVIS_BRANCH to UAT"
+  rm .env
+  mv .env-uat .env
+  rm -rf build
+  npm run build
+  cd build
+  zip -r barlingo-fe.zip .
+  cd ..
+  sshpass -p $DEPLOY_PASSWORD scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null build/barlingo-fe.zip $DEPLOY_USER@$DEPLOY_DOMAIN:~/barlingo-deploy-packages/barlingo-fe-uat/barlingo-fe.zip
   sshpass -p $DEPLOY_PASSWORD ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_DOMAIN "bash deploy-barlingo-fe.sh UAT $TRAVIS_BRANCH 1> ~/barlingo-deploy-logs/barlingo-fe-uat/deploy-$NOW.log 2>&1"
 elif [ "$TRAVIS_TAG" != "" ]; then
   echo "Deploying $TRAVIS_TAG to PRD"
+  rm .env
+  mv .env-prd .env
+  rm -rf build
+  npm run build
+  cd build
+  zip -r barlingo-fe.zip .
+  cd ..
+  sshpass -p $DEPLOY_PASSWORD scp -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null build/barlingo-fe.zip $DEPLOY_USER@$DEPLOY_DOMAIN:~/barlingo-deploy-packages/barlingo-fe-prd/barlingo-fe.zip
   sshpass -p $DEPLOY_PASSWORD ssh -o StrictHostKeyChecking=no $DEPLOY_USER@$DEPLOY_DOMAIN "bash deploy-barlingo-fe.sh PRD $TRAVIS_TAG 1> ~/barlingo-deploy-logs/barlingo-fe-prd/deploy-V$TRAVIS_TAG-$NOW.log 2>&1"
 fi
 
