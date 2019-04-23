9282950ae7e6d827f7484db4e81e5797c8ba5cb5
José <dragojola@gmail.com>
2019-04-23 21:39:23 +0200

    #125 - @josgirrui

--- src/containers/ProfileView/ProfileView.js
+++ src/containers/ProfileView/ProfileView.js
@@ -1,18 +1,15 @@
-import { Avatar, Card, Button } from 'antd';
-import axios from "axios";
+import { Button, Card, Form, Icon, Input, Modal } from 'antd';
 import React, { Component } from 'react';
 import { withNamespaces } from "react-i18next";
 import { Page, Section } from "react-page-layout";
+import { Redirect } from 'react-router-dom';
 import { Col, Row } from 'reactstrap';
 import { auth } from '../../auth';
-import { userService } from '../../services/userService';
 import Loading from "../../components/Loading/Loading";
-import locationIcon from '../../media/imageedit_5_5395394410.png';
-import { Redirect } from 'react-router-dom';
 import defaultImage from '../../media/default-exchange-header.jpg';
-import FileUploadComponent from './../../components/FileUploadComponent/'
-import {aut} from './../../auth'
-
+import locationIcon from '../../media/imageedit_5_5395394410.png';
+import { userService } from '../../services/userService';
+import FileUploadComponent from './../../components/FileUploadComponent/';
 class ProfileView extends Component {
     constructor(props) {
         super(props);
@@ -21,7 +18,10 @@ class ProfileView extends Component {
             user: null,
             loaded: false,
             isLoggedUser: false,
-            errorMessage: null
+            errorMessage: null,
+            ModalText: null,
+            visible: false,
+            confirmLoading: false,
         }
     }
 
@@ -57,21 +57,21 @@ class ProfileView extends Component {
             this.setData(user, true)
         } else {
             userService.findById(userId)
-            .then((response) => this.setData(response.data, false))
-            .catch((error) => this.setError(error));
+                .then((response) => this.setData(response.data, false))
+                .catch((error) => this.setError(error));
 
         }
     }
 
     renderDescription() {
-        const{t} = this.props
+        const { t } = this.props
         let user = this.state.user
-        const {city, country, address = "", workingHours = "", birthday = "", email = "", description ="", offer = "" } = user
-        const address1 = city + ", " + country+", "+address;
+        const { city, country, address = "", workingHours = "", birthday = "", email = "", description = "", offer = "" } = user
+        const address1 = city + ", " + country + ", " + address;
 
         return (
             <div className="exchange">
-            <div>
+                <div>
                     {birthday}
                 </div>
                 <div>
@@ -84,15 +84,15 @@ class ProfileView extends Component {
                 <div>
                     {workingHours}
                 </div>
-                
+
                 <div>
                     {description}
                 </div>
                 <div>
                     {offer}
                 </div>
-                
-                
+
+
             </div>
         );
     }
@@ -101,11 +101,65 @@ class ProfileView extends Component {
         return (image === '' || image === null) ? defaultImage : image;
     };
 
+    borrarCuenta = () => {
+        this.showModal();
+    }
+
+    showModal = () => {
+
+        const { t } = this.props
+
+        const { getFieldDecorator } = this.props.form;
+        const modalText = (
+            <div>
+                <p>
+                    Esta acción no es reversible, y eliminará su cuenta y datos personales de manera definitiva del sistema.
+                </p>
+                <p>
+                    Para borrar los datos personales introduzca su contraseña.
+                </p>
+                <Form>
+                    <Form.Item >
+                        {getFieldDecorator('code', {
+                            rules: [{ required: true, message: t('code.validate.empty') }],
+                        })(
+                            <Input className={"customInput"} prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />} type="text" placeholder={t('form.pleaseconfirmpassword')} />
+                        )}
+                    </Form.Item>
+                </Form>
+            </div>
+        );
+        this.setState({
+            visible: true,
+            ModalText: modalText
+        });
+    }
+    handleInput = (event) => {
+
+    }
+    handleOk = () => {
+        this.setState({
+            ModalText: 'The modal will be closed after two seconds',
+            confirmLoading: true,
+        });
+        setTimeout(() => {
+            this.setState({
+                visible: false,
+                confirmLoading: false,
+            });
+        }, 2000);
+    }
+
+    handleCancel = () => {
+        console.log('Clicked cancel button');
+        this.setState({
+            visible: false,
+        });
+    }
     render() {
-        const {t} = this.props
+        const { t } = this.props
         const { Meta } = Card;
-        const { errorMessage, loaded, user, editProfile } = this.state;
-        console.log(user)
+        const { errorMessage, loaded, user, editProfile, visible, confirmLoading, ModalText } = this.state;
 
         if (editProfile) {
             return (<Redirect to={"/editProfile"} />);
@@ -127,30 +181,48 @@ class ProfileView extends Component {
                         <Col col-sm="12" offset-md="4" col-md="4">
                             <Card
                                 cover={
-                                    <FileUploadComponent allowUpload={auth.isUser()} imageType={"profileBackPic"} full={true} width={"auto"} height={300} endpoint={"/users/" + auth.getUserData().id + "/upload?imageType=backPic"} imageUrl = { auth.isUser() ? user.profileBackPic: user.images[0] } defaultImage={defaultImage} />
-                                    }>
+                                    <FileUploadComponent allowUpload={auth.isUser()} imageType={"profileBackPic"} full={true} width={"auto"} height={300} endpoint={"/users/" + auth.getUserData().id + "/upload?imageType=backPic"} imageUrl={auth.isUser() ? user.profileBackPic : user.images[0]} defaultImage={defaultImage} />
+                                }>
                                 <Meta
                                     avatar={
-                                        <FileUploadComponent allowUpload={auth.isUser()} imageType={"personalPic"} width={40} height={"auto"} endpoint={"/users/" + auth.getUserData().id + "/upload?imageType=personal"} imageUrl = { auth.isUser() ? user.personalPic: user.imageProfile} defaultImage={defaultImage} />
+                                        <FileUploadComponent allowUpload={auth.isUser()} imageType={"personalPic"} width={40} height={"auto"} endpoint={"/users/" + auth.getUserData().id + "/upload?imageType=personal"} imageUrl={auth.isUser() ? user.personalPic : user.imageProfile} defaultImage={defaultImage} />
 
-                                }
-                                    title={auth.isUser() ? user.name:user.establishmentName}
+                                    }
+                                    title={auth.isUser() ? user.name : user.establishmentName}
                                     description={<div>{this.renderDescription()}</div>}
-                                    
+
                                 />
 
-
-                                {user.id === auth.getUserData().id && <Button type="primary" onClick={() => this.setState({ editProfile: true })} htmlType="submit" className="login-form-button primaryButton">
-                                    {t('edit')}
-                                </Button>}
+                                <Row>
+                                    <Col xs="1">
+                                        {user.id === auth.getUserData().id && <Button type="primary" onClick={() => this.setState({ editProfile: true })} htmlType="submit" className="login-form-button primaryButton">
+                                            {t('edit')}
+                                        </Button>}
+                                    </Col>
+                                    <Col xs="1">
+                                        {user.id === auth.getUserData().id && <Button type="primary" onClick={() => this.borrarCuenta()} htmlType="submit" className="login-form-button primaryButton">
+                                            {t('deleteAccount.delete')}
+                                        </Button>}
+                                    </Col>
+                                </Row>
                             </Card>
-                            
+
                         </Col>
                     </Row>
+                    <Modal
+                        title={t('deleteAccount.delete')}
+                        visible={visible}
+                        onOk={this.handleOk}
+                        confirmLoading={confirmLoading}
+                        onCancel={this.handleCancel}
+                    >
+                        <p>{ModalText}</p>
+                    </Modal>
                 </Section>
             </Page >
         );
     }
 }
 
+ProfileView = Form.create({ name: "password" })(ProfileView);
 export default withNamespaces('translation')(ProfileView);
